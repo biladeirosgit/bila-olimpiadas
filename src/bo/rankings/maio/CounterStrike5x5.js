7c6a405c14b12e98b10f39ca5230b17dccc6c766
bila-olimpiadas <bilaolimpiadas@gmail.com>
2024-06-08 22:23:46 +0100

    jd

--- src/bo/rankings/maio/CounterStrike5x5.js
+++ src/bo/rankings/maio/CounterStrike5x5.js
@@ -4,12 +4,47 @@ import Footer from "../../Footer";
 
 const CounterStrike5x5 = () => {
 
+    var player1 = { "#" : "1º" ,Nome: "Matadores dos Cheios de Fome" , Membros : "Squnha, Mestre Diogo, Rodry, Cludos, Xavier"          , Pt: "+8"};
+    var player2 = { "#" : "2º" ,Nome: "ReisDelas"                    , Membros : "Lemos, Gelly, Hydra, Tone & Lekky"             , Pt: "+6"};
+    var player3 = { "#" : "3º" ,Nome: "Os Sportinguistas do Sardoal" , Membros : "Wisdow, Sardoal, João, João Tavares & Lobato"  , Pt: "+4"};
+    var player4 = { "#" : "4º" ,Nome: "Os Fora da Lei"               , Membros : "Geremias, Alcunha, Braz, João Nuno & Xadas"    , Pt: "+2"};
+    
+    var players = [player1, player2, player3, player4];
+
     return (
         <>
             <Navebar />
             <div className="w3-container w3-light-gray" style={{ textAlign: "center" }}>
                 <div className="w3-container w3-center" style={{ width: "60%", display: "inline-block" }}>
-                    <p><b>Ainda não finalizado!</b></p>
+                    <h1 className="w3-center">Rankings do Rocket League 3x3</h1>
+                    <div className="rankings-container">
+                        <table className="rankings-table w3-table w3-centered w3-table-all w3-hoverable">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nome</th>
+                                    <th>Membros</th>
+                                    <th>Pts</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {players.map((player, index) => (
+                                    <tr key={index}>
+                                        <td>{player["#"]}</td>
+                                        <td>{player.Nome}</td>
+                                        <td>{player.Membros}</td>
+                                        <td>{player.Pt}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="w3-container">
+                    <br></br>
+                    <p>
+                      <b>Pts</b> - Pontos Ganhos
+                    </p>
                 </div>
                 <br></br>
                 <div class="w3-container">
