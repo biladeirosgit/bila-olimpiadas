de2604a748ef997dbc89102bb0efe4d0302b40e3
bila-olimpiadas <bilaolimpiadas@gmail.com>
2024-07-21 14:58:41 +0100

    ping pong filtro presencial

--- src/bo/rankings/Rankings.js
+++ src/bo/rankings/Rankings.js
@@ -119,7 +119,8 @@ const Rankings = () => {
             "Sueca",
             "Bilabilhar",
             "Basquetiladas",
-            "EscapeRoom"
+            "EscapeRoom",
+            "Ping Pong"
         ]
     }
 
